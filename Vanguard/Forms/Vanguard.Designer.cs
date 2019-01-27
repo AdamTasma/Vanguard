27a2da21216bb83c7df7b47bdd48f6cf21780c51
Adam Tasma <adamtasma@gmail.com>
2019-01-27 18:22:35 -0500

    working pages

--- Vanguard/Forms/Vanguard.Designer.cs
+++ Vanguard/Forms/Vanguard.Designer.cs
@@ -29,32 +29,57 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnP3Previous = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.btnP1Next = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnP2Next = new System.Windows.Forms.Button();
             this.btnP2Previous = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnP3Previous = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.btnP1Next);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(41, 29);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(334, 294);
+            this.panel1.Size = new System.Drawing.Size(274, 187);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnP3Previous);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(367, 164);
+            this.panel3.TabIndex = 2;
+            this.panel3.Visible = false;
+            // 
+            // btnP3Previous
+            // 
+            this.btnP3Previous.Location = new System.Drawing.Point(33, 129);
+            this.btnP3Previous.Name = "btnP3Previous";
+            this.btnP3Previous.Size = new System.Drawing.Size(75, 23);
+            this.btnP3Previous.TabIndex = 1;
+            this.btnP3Previous.Text = "Previous";
+            this.btnP3Previous.UseVisualStyleBackColor = true;
+            this.btnP3Previous.Click += new System.EventHandler(this.btnP3Previous_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "page 3";
             // 
             // btnP1Next
             // 
@@ -65,15 +90,6 @@
             this.btnP1Next.Text = "Next";
             this.btnP1Next.UseVisualStyleBackColor = true;
             this.btnP1Next.Click += new System.EventHandler(this.btnP1Next_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 239);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Please click next to continue";
             // 
             // label1
             // 
@@ -91,8 +107,9 @@
             this.panel2.Controls.Add(this.label3);
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(335, 294);
+            this.panel2.Size = new System.Drawing.Size(265, 203);
             this.panel2.TabIndex = 1;
+            this.panel2.Visible = false;
             // 
             // btnP2Next
             // 
@@ -123,50 +140,23 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "page 2";
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.btnP3Previous);
-            this.panel3.Controls.Add(this.label4);
-            this.panel3.Location = new System.Drawing.Point(41, 29);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(335, 294);
-            this.panel3.TabIndex = 2;
-            // 
-            // btnP3Previous
-            // 
-            this.btnP3Previous.Location = new System.Drawing.Point(33, 129);
-            this.btnP3Previous.Name = "btnP3Previous";
-            this.btnP3Previous.Size = new System.Drawing.Size(75, 23);
-            this.btnP3Previous.TabIndex = 1;
-            this.btnP3Previous.Text = "Previous";
-            this.btnP3Previous.UseVisualStyleBackColor = true;
-            this.btnP3Previous.Click += new System.EventHandler(this.btnP3Previous_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 30);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "page 3";
-            // 
             // Vanguard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
             this.Name = "Vanguard";
             this.Text = "Vanguard";
             this.Load += new System.EventHandler(this.Vanguard_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -175,7 +165,6 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnP1Next;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnP2Next;
--- Vanguard/Forms/Vanguard.cs
+++ Vanguard/Forms/Vanguard.cs
@@ -19,20 +19,15 @@ namespace Vanguard.Forms
 
         private void Vanguard_Load(object sender, EventArgs e)
         {
-            panel2.Hide();
-            panel3.Hide();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            //https://www.youtube.com/watch?v=5Anto1N_FSs
+            panel2.Visible = false;
+            panel3.Visible = false;
         }
 
         private void btnP1Next_Click(object sender, EventArgs e)
         {
-            panel1.Show();
+            panel1.Visible = false;
             panel2.BringToFront();
-            panel2.Show();
+            panel2.Visible = true;
         }
 
         private void btnP2Previous_Click(object sender, EventArgs e)
