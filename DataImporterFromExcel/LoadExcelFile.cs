fe42638fe7247567626ae34f138531f87383b678
Adam Tasma <adamtasma@gmail.com>
2019-03-31 23:07:51 -0400

    add VanguardTests with nunit and a TestModel

--- DataImporterFromExcel/LoadExcelFile.cs
+++ DataImporterFromExcel/LoadExcelFile.cs
@@ -13,19 +13,107 @@ namespace DataImporterFromExcel
 
         public void LoadAllTabs(string filePath)
         {
+            List<TestModel> myTestModels = GetTestModels(filePath);
+
+
             using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, true))
             {
-                GetDbVersionNumber(doc, 4);
+                //GetDbVersionNumber(doc, 4);
                 GetWeapons(doc, 5);
                 GetPlotArmor(doc, 6);
             }
-
         }
 
-        private int GetDbVersionNumber(SpreadsheetDocument doc, int dbTab)
+        public List<TestModel> GetTestModels(string filePath)
         {
-            return 0;
+            List<TestModel> myTestModels = new List<TestModel>();
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, true))
+            {
+                WorkbookPart workbookPart = doc.WorkbookPart;
+
+                var stringTablePart = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.ElementAt(7); // This is the "Traits" tab;
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+
+                ArrayList data = new ArrayList();
+
+                List<Row> rows = sheetData.Elements<Row>().ToList();
+
+                rows.RemoveAt(0);
+
+                string category = "";
+
+                foreach (Row r in rows)
+                {
+                    TestModel ctm = new TestModel();
+                    List<string> cellValues = new List<string>();
+                    foreach (Cell c in r.Elements<Cell>())
+                    {
+                        if (c.InnerText != "")
+                        {
+                            if (c.DataType == null)
+                            {
+                                cellValues.Add(c.InnerText);
+                            }
+                            else
+                            {
+                                cellValues.Add(stringTablePart.SharedStringTable.ElementAt(int.Parse(c.InnerText)).InnerText);
+                            }
+                        }
+                    }
+                    //if (cellValues.Count == 1)
+                    //{
+                    //    category = cellValues.ElementAt(0);
+                    //}
+                    //else if (cellValues.Count > 1)
+                    if (cellValues.Count > 1)
+                    {
+                        ctm.TestString = cellValues.ElementAt(0);
+                        //ctm.TestInt = cellValues.ElementAt(1);
+                        
+                        myTestModels.Add(ctm);
+                    }
+                }
+            }
+            return myTestModels;
         }
+
+        private string GetDbVersionNumber(SpreadsheetDocument doc, int dbTab)
+        {
+            WorkbookPart workbookPart = doc.WorkbookPart;
+            var stringTablePart = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+            WorksheetPart worksheetPart = workbookPart.WorksheetParts.ElementAt(dbTab);
+            SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+            ArrayList data = new ArrayList();
+            List<Row> rows = sheetData.Elements<Row>().ToList();
+            rows.RemoveAt(0); //skips header
+
+            string category = "";
+
+            string version;
+            foreach (Row r in rows)
+            {
+                List<string> cellValues = new List<string>();
+                foreach (Cell c in r.Elements<Cell>())
+                {
+                    if (c.InnerText != "")
+                    {
+                        if (c.DataType == null)
+                        {
+                            cellValues.Add(c.InnerText);
+                        }
+                        else
+                        {
+                            cellValues.Add(stringTablePart.SharedStringTable.ElementAt(int.Parse(c.InnerText)).InnerText);
+                        }
+                    }
+                }
+                version = cellValues.ElementAt(0);
+            }
+            return "temp";
+        }
+
 
         private List<Row> ReadFromSheet(SpreadsheetDocument doc, int dbTab)
         {
@@ -73,62 +161,59 @@ namespace DataImporterFromExcel
             return null;
         }
 
-        public string myVar = "hello" + ;
+        //public List<Trait> GetTraits(string filePath)
+        //{
+        //    List<Trait> traitList = new List<Trait>();
+        //    using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, true))
+        //    {
+        //        WorkbookPart workbookPart = doc.WorkbookPart;
 
+        //        var stringTablePart = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
 
-        public List<Trait> GetTraits(string filePath)
-        {
-            List<Trait> traitList = new List<Trait>();
-            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, true))
-            {
-                WorkbookPart workbookPart = doc.WorkbookPart;
+        //        WorksheetPart worksheetPart = workbookPart.WorksheetParts.ElementAt(2); // This is the "Traits" tab;
+        //        SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
 
-                var stringTablePart = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+        //        ArrayList data = new ArrayList();
 
-                WorksheetPart worksheetPart = workbookPart.WorksheetParts.ElementAt(2); // This is the "Traits" tab;
-                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+        //        List<Row> rows = sheetData.Elements<Row>().ToList();
 
-                ArrayList data = new ArrayList();
+        //        rows.RemoveAt(0);
 
-                List<Row> rows = sheetData.Elements<Row>().ToList();
+        //        string category = "";
 
-                rows.RemoveAt(0);
-
-                string category = "";
-
-                foreach (Row r in rows)
-                {
-                    Trait relevantTrait = new Trait();
-                    List<string> cellValues = new List<string>();
-                    foreach (Cell c in r.Elements<Cell>())
-                    {
-                        if (c.InnerText != "")
-                        {
-                            if (c.DataType == null)
-                            {
-                                cellValues.Add(c.InnerText);
-                            }
-                            else
-                            {
-                                cellValues.Add(stringTablePart.SharedStringTable.ElementAt(int.Parse(c.InnerText)).InnerText);
-                            }
-                        }
-                    }
-                    if (cellValues.Count == 1)
-                    {
-                        category = cellValues.ElementAt(0);
-                    }
-                    else if (cellValues.Count > 1)
-                    {
-                        relevantTrait.category = category;
-                        relevantTrait.traitName = cellValues.ElementAt(0);
-                        relevantTrait.costInD = cellValues.ElementAt(1);
-                        relevantTrait.description = cellValues.ElementAt(2);
-                        traitList.Add(relevantTrait);
-                    }
-                }
-            }
-            return traitList;
-        }
+        //        foreach (Row r in rows)
+        //        {
+        //            Trait relevantTrait = new Trait();
+        //            List<string> cellValues = new List<string>();
+        //            foreach (Cell c in r.Elements<Cell>())
+        //            {
+        //                if (c.InnerText != "")
+        //                {
+        //                    if (c.DataType == null)
+        //                    {
+        //                        cellValues.Add(c.InnerText);
+        //                    }
+        //                    else
+        //                    {
+        //                        cellValues.Add(stringTablePart.SharedStringTable.ElementAt(int.Parse(c.InnerText)).InnerText);
+        //                    }
+        //                }
+        //            }
+        //            if (cellValues.Count == 1)
+        //            {
+        //                category = cellValues.ElementAt(0);
+        //            }
+        //            else if (cellValues.Count > 1)
+        //            {
+        //                relevantTrait.category = category;
+        //                relevantTrait.traitName = cellValues.ElementAt(0);
+        //                relevantTrait.costInD = cellValues.ElementAt(1);
+        //                relevantTrait.description = cellValues.ElementAt(2);
+        //                traitList.Add(relevantTrait);
+        //            }
+        //        }
+        //    }
+        //    return traitList;
+        //}
     }
 }
---
+++ Vanguard/Models/TestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanguard.Models
+{
+    public class TestModel
+    {
+        public string TestString { get; set; }
+        public int TestInt { get; set; }
+    }
+}
